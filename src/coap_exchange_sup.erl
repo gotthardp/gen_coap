01a4f73e1c955473cf3024ef6e7ca3d170571a5e
Petr Gotthard <petr.gotthard@honeywell.com>
2015-08-16 22:16:21 +0200

    Change lincese to MPL 1.1.

--- src/coap_exchange_sup.erl
+++ src/coap_exchange_sup.erl
@@ -1,9 +1,10 @@
 %
-% This Source Code Form is subject to the terms of the Mozilla Public
-% License, v. 2.0. If a copy of the MPL was not distributed with this
-% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+% The contents of this file are subject to the Mozilla Public License
+% Version 1.1 (the "License"); you may not use this file except in
+% compliance with the License. You may obtain a copy of the License at
+% http://www.mozilla.org/MPL/
 %
-% Copyright (C) 2015 Petr Gotthard <petr.gotthard@centrum.cz>
+% Copyright (c) 2015 Petr Gotthard <petr.gotthard@centrum.cz>
 %
 
 % supervisor for coap_exchange_inbound and _outbound
